4dd93cb404bc0a314e7bf00609aee9f5b0659b58
Andrew Owino <andystudy4@gmail.com>
2024-07-21 22:34:14 +0300

    Default to 'metric'

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -13,7 +13,14 @@ class WeatherController extends Controller
     public function getWeather(Request $request, GeocodingService $geocodingService, WeatherService $weatherService): JsonResponse {
 
         $city = $request->query('city');
-        $units = $request->query('units');
+        $units = $request->query('units'); 
+
+        $allowedUnits = ['metric', 'standard', 'imperial'];
+
+        if (!in_array($units, $allowedUnits)) {
+            // Default to 'metric'
+            $units = 'metric'; 
+        }
         
         if (!$city) {
             return response()->json(["error" => "City is required"], 400);
